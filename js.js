4571024d392c56fb812ef58baf325960e3feba8f
your name <manhnguyen.tla@gmail.com>
2022-07-21 19:47:07 +0700

    S

---
+++ js.js
@@ -0,0 +1,16 @@
+function result() {
+    var year = document.querySelector('#year').value;
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+          if (year % 400 == 0) {
+            alert(year + " is a leap year");
+          } else {
+            alert(year + " is NOT a leap year");
+          }
+        } else {
+          alert(year + " is a leap year");
+        }
+      } else {
+        alert(year + " is NOT a leap year");
+      }
+}
